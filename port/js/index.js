19d6a8f2553b630a5aa05a1177b99961b5230342
jdj <wowv333@gmail.com>
2024-12-07 16:45:41 +0900

    slide 수정

--- port/js/index.js
+++ port/js/index.js
@@ -80,151 +80,76 @@ $(function () {
   });
 
   /* #design1 (start) ****************************************/
-  let design1_slide = $("#design1 .contWrap .cont li").outerWidth(true);
-  let design1_index = 0;
-  let design_count = $("#design1 .contWrap .cont li").length - 1;
 
-  // 삭제
-  // $(window).resize(function(){
-  //     clearInterval(design1_intervalId);
-  //     design1_slide = $(this).width();
-  //     $("#design1 .contWrap .cont").stop().animate({
-  //         left: -design1_slide*design1_index,
-  //     },0)
-  //     design1_intervalId= setInterval(slideMove,3000);
-  // })
+          var design1 = new Swiper("#design1 .contWrap", {
+            slidesPerView: 3,
+            centeredSlides: true,
+            spaceBetween: 40,
+            autoplay: {
+              delay: 3000
+            },
 
-  // 다음 슬라이드
-  function nextSlide() {
-    if (design1_index < design_count) {
-      design1_index++;
-    } else {
-      design1_index = 0;
-    }
-  }
+            loop : true,
+            pagination: {
+              el: ".swiper-pagination",
+              type: "fraction",
+            },
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+            // 반응형 스와이퍼 
+            breakpoints: {
+              400: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              // 768: {
+              //   slidesPerView: 2,
+              //   spaceBetween: 40,
+              // },
+              // 1024: {
+              //   slidesPerView: 3,
+              //   spaceBetween: 50,
+              // },
+            },     
+          });
+          // pop
+      $("#design1 .cont li a").click(function (e) {
+        e.preventDefault();
+        design1.autoplay.stop();
+        $(this).siblings(".pop").css({ display: "block" });
+      });
+      $("#design1 .pop button").click(function () {
+        design1.autoplay.start();
+        $(".pop").css({ display: "none" });
+        $("#design1 .btn .control li").removeClass("on");
+        $("#design1 .btn .control .start").addClass("on");
+      });
+      $("#design1 .pop").click(function (event) {
+        if (event.target === this) {
+          design1.autoplay.start();
+          $(".pop").css({ display: "none" });
+        }
+      });
 
-  // 현재 슬라이드, 롤링에만 class 적용
-  $("#design1 .cont li").eq(design1_index).addClass("on");
-  $("#design1 .rolling li").eq(design1_index).addClass("on");
-
-  function design1_class() {
-    $("#design1 .cont li").removeClass("on");
-    $("#design1 .cont li")
-      .eq(design1_index + 1)
-      .addClass("on");
-    $("#design1 .rolling li").removeClass("on");
-    $("#design1 .rolling li").eq(design1_index).addClass("on");
-  }
-
-  // 슬라이드 움직이는 거리
-  function slideMove() {
-    nextSlide();
-    $("#design1 .contWrap .cont")
-      .stop()
-      .animate(
-        {
-          left: -design1_slide * design1_index,
-        },
-        500
-      );
-    design1_class(); //클래스 toggle 함수
-  }
-  // 자동 슬라이드 + 정지 시키기 위해 변수에 저장
-  let design1_intervalId = setInterval(slideMove, 4000);
-
-  //맨앞, 맨뒤 가상요소 추가
-  let current_width = $(window).width();
-  if (current_width > 1280) {
-    let design_item1 = $("#design1 .cont li").first().html(); //맨앞 슬라이드 저장
-    let design_item2 = $("#design1 .cont li").last().html(); //맨뒤 슬라이드 저장
-
-    function addElement() {
-      $("#design1 .cont").prepend("<li>" + design_item2 + "</li>");
-      $("#design1 .cont").append("<li>" + design_item1 + "</li>");
-    }
-    addElement();
-  }
-
-  // 롤링 클릭시 해당 슬라이드로 이동
-  $("#design1 .rolling li").click(function () {
-    clearInterval(design1_intervalId);
-    design1_index = $(this).index();
-    $("#design1 .contWrap .cont")
-      .stop()
-      .animate(
-        {
-          left: -design1_slide * design1_index,
-        },
-        0
-      );
-
-    design1_class(); //클래스 toggle 함수
-
-    design1_intervalId = setInterval(slideMove, 3000);
-  });
-
-  $("#design1 .btn .control li").click(function () {
-    $("#design1 .btn .control li").removeClass("on");
-    $(this).addClass("on");
-  });
-  // 슬라이드 정지 버튼
-  $("#design1 .btn .pause").click(function () {
-    clearInterval(design1_intervalId);
-  });
-
-  // 슬라이드 재생 버튼
-  $("#design1 .btn .start").click(function () {
-    clearInterval(design1_intervalId);
-    setTimeout(() => {
-      slideMove();
-      design1_intervalId = setInterval(slideMove, 3000);
-    }, 500);
-  });
-
-  // pop
-  $("#design1 .cont li a").click(function (e) {
-    e.preventDefault();
-    $(this).siblings(".pop").css({ display: "block" });
-    clearInterval(design1_intervalId);
-  });
-  $("#design1 .pop button").click(function () {
-    clearInterval(design1_intervalId);
-    design1_intervalId = setInterval(slideMove, 1000);
-    $(".pop").css({ display: "none" });
-    $("#design1 .btn .control li").removeClass("on");
-    $("#design1 .btn .control .start").addClass("on");
-  });
-  $("#design1 .pop").click(function (event) {
-    if (event.target === this) {
-      $(".pop").css({ display: "none" });
-    }
-    clearInterval(design1_intervalId);
-    design1_intervalId = setInterval(slideMove, 3000);
-    $("#design1 .btn .control li").removeClass("on");
-    $("#design1 .btn .control .start").addClass("on");
-  });
-
-  if (w >= 768) {
-    let current_src; //현재 hover 된 이미지 경로 저장할 변수
-    let imgName; //확장자 없이 이미지의 이름만 저장할 변수
-
-    $("#design1 .contWrap li").click(function () {
-      current_src = $(this).find("a>img").attr("src"); //이미지 경로를 가져옴
-      /* current_src =  ./img/post/up/sanmobile.jpg */
-      imgName = current_src
-        .split("/")
-        .pop()
-        .replace(/\.[^/.]+$/, ""); //파일 확장자 없이 이름만 추출
-      /* imgNmae = sanmobile */
-      console.log(imgName);
-      $(this)
-        .find(".pop img")
-        .attr({ src: "./img/post/up/" + imgName + "_pc.jpg" });
-      /* sanmobile_pc.jpg */
-    });
-  }
+          
 
   /* #design1 (end) ****************************************/
+
+  /* #web(start) ****************************************/
+  var web = new Swiper(".web", {
+    pagination: {
+      el: ".web .swiper-pagination",
+      type: "bullets",
+    },
+    navigation: {
+      nextEl: ".web .swiper-button-next",
+      prevEl: ".web .swiper-button-prev",
+    },
+  });
+    /* #web(end) ****************************************/
+
 
   $("#dpage .cont1 li").hover(
     function () {
